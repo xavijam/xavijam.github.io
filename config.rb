6bbeb275505f0400bba26a5dcd2f6518f91ed070
xavijam <xavijam@gmail.com>
2014-01-06 00:31:19 +0100

    first changes

--- config.rb
+++ config.rb
@@ -5,14 +5,14 @@ project_type = :stand_alone
 
 # Publishing paths
 http_path = "/"
-http_images_path = "/images"
-http_generated_images_path = "/images"
+http_images_path = "/assets/img"
+http_generated_images_path = "/assets/img"
 http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+css_dir = "source/css"
 
 # Local development paths
 sass_dir = "sass"
-images_dir = "source/images"
+images_dir = "source/assets/img"
 fonts_dir = "source/fonts"
 
 line_comments = false
