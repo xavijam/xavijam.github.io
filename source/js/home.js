c0162b9240664f063d565882818b134e0b8bf406
Javier Álvarez Medina <xavijam@gmail.com>
2014-11-19 21:28:41 +0100

    Clean is the way

--- source/js/home.js
+++
@@ -1,7 +0,0 @@
-//= require vendor/d3.v3.min.js
-//= require vendor/topojson.v1.min.js
-
-//= require models/milestone.js
-//= require models/user.js
-//= require_tree ../../source/js/projects
-//= require_tree ../../source/js/home
