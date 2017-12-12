9c36509fc2eed0c5a629b8d384fe3068ea811213
Javier Álvarez Medina <xavijam@gmail.com>
2017-12-12 10:17:35 +0100

    Blog js entry added + removed unnecessary postcss packages

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,8 @@ export default {
 
   context: path.join(__dirname, "src"),
   entry: {
-    app: ["./js/app"]
+    app: ["./js/app"],
+    blog: ["./js/blog"]
   },
   output: {
     path: path.join(__dirname, "public/js"),
